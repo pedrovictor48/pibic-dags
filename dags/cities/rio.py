78f06c9d29d2926f743119b6ef7bfaa5a4137dab
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2022-09-27 17:42:14 -0300

    Update rio.py

--- dags/cities/rio.py
+++ dags/cities/rio.py
@@ -10,17 +10,12 @@ def req() -> dict:
 def process_response(response: dict) -> list:
     lista_onibus = list()
     for vs in response:
-        instancia_processada = {
-            'latitude' : vs['laitutde'],
-            'longitude' : vs['longitude'],
-            'tempo_captura' : vs['dataHora'],
-            'id_onibus' : vs['ordem']
-        }
-        lista_onibus.append(instancia_processada)
+        lista_onibus.append(vs)
+        
     return lista_onibus
 
 
 def rio():
     response = req();
-    processed_response = process_response(response)
-    postar(process_response)
+    response_list = process_response(response)
+    postar(response_list)
