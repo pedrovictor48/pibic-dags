7f7270947df442bd9a8cba479d915e1d57c4456d
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2022-09-27 17:46:37 -0300

    Create curitiba.py

---
+++ dags/cities/curitiba.py
@@ -0,0 +1,22 @@
+import requests
+from cities.utils import postar
+
+URL = 'https://transporteservico.urbs.curitiba.pr.gov.br/getVeiculos.php?&c=e5b1c'
+
+def req() -> dict:
+    response = requests.get(URLAPI + "/Posicao", cookies=auth.cookies)
+    response = response.json()
+
+    return response;
+
+def process_response(response: dict) -> list:
+    lista_onibus = list()
+    for cod in response:
+        lista_onibus.append(response[cod])
+    
+    return lista_onibus
+
+def curitiba():
+    response = req();
+    response_list = process_response(response)
+    postar(response_list)
