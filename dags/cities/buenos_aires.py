6e4020b7d55666dea24ddcb2e50ec59b0bb847ac
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2022-09-28 22:13:15 -0300

    Create buenos_aires.py

---
+++ dags/cities/buenos_aires.py
@@ -0,0 +1,28 @@
+import requests
+from cities.utils import postar
+
+CSV_LINK = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/transporte-y-obras-publicas/flujo-vehicular-anillo-digital/dataset_flujo_vehicular.csv'
+
+def process_response(dados):
+    linhas = dados.split('\n')
+    linhas = linhas[1:-1]
+    
+    veiculos = []
+    for linha in linhas:
+        valores = linha.split(',')
+        veiculo = dict()
+
+        veiculo['latitude'] = valores[4]
+        veiculo['longitude'] = valores[5]
+        veiculo['hora'] = valores[1]
+        veiculo['id'] = valores[0]
+
+        veiculos.append(veiculo)
+    
+    return veiculos
+
+def buenos_aires():
+    csv_req = requests.get(CSV_LINK)
+    dados = csv_req.content.decode('utf-8-sig')
+    processed_response = process_response(dados)
+    postar(processed_response)
